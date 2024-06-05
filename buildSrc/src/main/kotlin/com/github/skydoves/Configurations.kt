06dc15224bafa46807c53aaac2a9ab0e7cfe33df
skydoves <skydoves@naver.com>
2024-06-05 17:12:56 +0900

    Prepare for release 1.0.5

--- buildSrc/src/main/kotlin/com/github/skydoves/Configurations.kt
+++ buildSrc/src/main/kotlin/com/github/skydoves/Configurations.kt
@@ -22,9 +22,9 @@ object Configurations {
   const val minSdk = 21
   const val majorVersion = 1
   const val minorVersion = 0
-  const val patchVersion = 4
+  const val patchVersion = 5
   const val versionName = "$majorVersion.$minorVersion.$patchVersion"
-  const val versionCode = 5
+  const val versionCode = 6
   const val snapshotVersionName = "$majorVersion.$minorVersion.${patchVersion + 1}-SNAPSHOT"
   const val artifactGroup = "com.github.skydoves"
 }
